df60c7efe4d53961b6b135994dbc5a424abbee51
Erik Đuranec <erik.djuranec@gmail.com>
2021-06-08 22:33:36 +0200

    Dodan ClassLibrary za funkcionalnost Rezervacije te klasa Rezervacija s pripadnim svojstvima.

---
+++ Software/RecenzijeLib/Rezervacija.cs
@@ -0,0 +1,25 @@
+﻿using FindAndLearn.Klase;
+using KorisniciLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RezervacijeLib
+{
+    public class Rezervacija
+    {
+        public int IdRezervacije { get; set; }
+        public Student Student { get; set; }
+        public Termin Termin { get; set; }
+        public DateTime RokRezervacije { get; set; }
+        public DateTime DatumRezervacije { get; set; }
+        public bool Potvrdjena { get; set; }
+
+        public Rezervacija()
+        {
+
+        }
+    }
+}
